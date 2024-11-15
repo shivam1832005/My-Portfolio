490c93801e516eeb07fdda9287820f263ab12449
shivam1832005 <smittal1832005@gmail.com>
2024-11-15 10:16:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -73,3 +73,18 @@ particlesJS("home", {
     },
     retina_detect: true
 });
+window.addEventListener("load", function() {
+    const loader = document.getElementById("loader");
+    loader.style.display = "none";
+});
+window.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom >= 0) {
+            section.classList.add('visible');
+        } else {
+            section.classList.remove('visible');
+        }
+    });
+});
